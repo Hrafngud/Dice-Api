4aa167bf130bfc9741a7f5a7c919bd69aa2e002c
hrafngud <joao.davi12121@gmail.com>
2025-04-28 01:00:56 -0300

    port number hotfix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors'); // Require cors
 const rollRoutes = require('./src/routes/rollRoutes.js');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // Enable CORS for all routes
 app.use(cors());
